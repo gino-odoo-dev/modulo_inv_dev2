e63af4608b4885f55ef07209f44dce6d94d832ca
marcoacevedoginodev <macevedo@gino.cl>
2025-02-18 11:25:18 -0300

    modulo etiquetas terminado primera etapa sin opciones de modelos

--- inv_dev/models/product_extension_wizard.py
+++ inv_dev/models/product_extension_wizard.py
@@ -1,51 +1,44 @@
 from odoo import models, fields, api, _
-import os 
-import logging 
+import logging
 import requests
-import shutil
 import base64
 from odoo.exceptions import UserError
 
-_logger = logging.getLogger(__name__) 
+_logger = logging.getLogger(__name__)
 
 class Codigo(models.Model):
-    _name = 'product.codigo'  
-    _description = 'Codigo'  
-    name = fields.Char(string="Articulo")
+    _name = 'product.codigo'
+    _description = 'Código'
+    name = fields.Char(string="Artículo")
 
 
-class Cantidad(models.Model):    
-    _name = 'product.cantidad' 
-    _description = 'Cantidad' 
-    name = fields.Float(string="Cantidad", default=0.0)  
+class Cantidad(models.Model):
+    _name = 'product.cantidad'
+    _description = 'Cantidad'
+    name = fields.Float(string="Cantidad", default=0.0)
 
 
 class Numero(models.Model):
-    _name = 'cl.product.tallas'  
-    _description = 'Tallas'   
+    _name = 'cl.product.tallas'
+    _description = 'Tallas'
     name = fields.Char(string="Nro. Zapato")
-    company_id = fields.Many2one('res.company', string="Company") 
+    company_id = fields.Many2one('res.company', string="Compañía")
 
 
 class ProductExtensionWizard(models.TransientModel):
     _name = 'product.extension.wizard'
     _description = 'Product Extension Wizard'
 
-    id_codigo = fields.Many2one('product.codigo', string="Codigo", required=True)
+    id_codigo = fields.Many2one('product.codigo', string="Código", required=True)
     id_numero = fields.Many2one('cl.product.tallas', string="Tallas", required=True)
     cantidad = fields.Integer(string="Cantidad", default=0, required=True)
-    zpl_content = fields.Text(string="ZPL Content", readonly=True)
     pdf_file = fields.Binary(string="Etiqueta en PDF", readonly=True)
     pdf_filename = fields.Char(string="Nombre del Archivo")
 
-    @api.model
-    def generate_zpl_label(self, vals):
-        codigo_record = self.env['product.codigo'].browse(vals.get('id_codigo'))
-        numero_record = self.env['cl.product.tallas'].browse(vals.get('id_numero'))
-
-        codigo = codigo_record.name if codigo_record else 'Desconocido'
-        numero = numero_record.name if numero_record else 'Desconocido'
-        cantidad = vals.get('cantidad', 0)
+    def generate_zpl_label(self):
+        codigo = self.id_codigo.name if self.id_codigo else 'Desconocido'
+        numero = self.id_numero.name if self.id_numero else 'Desconocido'
+        cantidad = self.cantidad
 
         zpl = """
         ^XA
@@ -66,37 +59,12 @@ class ProductExtensionWizard(models.TransientModel):
 
         return zpl.strip()
 
-    def create_and_generate_zpl(self):
-        vals = {
-            'id_codigo': self.id_codigo.id if self.id_codigo else '',
-            'id_numero': self.id_numero.id if self.id_numero else '',
-            'cantidad': self.cantidad
-        }
-
-        zpl = self.generate_zpl_label(vals)
-
-        wizard = self.create({
-            'id_codigo': self.id_codigo.id,
-            'id_numero': self.id_numero.id,
-            'cantidad': self.cantidad,
-            'zpl_content': zpl
-        })
-
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'product.extension.wizard',
-            'view_mode': 'form',
-            'res_id': wizard.id,
-            'target': 'new',
-        }
-
     def generate_pdf_from_zpl(self):
-        if not self.zpl_content:
-            raise UserError(_("No hay contenido para generar etiqueta"))
+        zpl = self.generate_zpl_label()
 
         url = 'http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/'
         headers = {'Accept': 'application/pdf'}
-        files = {'file': self.zpl_content.encode('utf-8')}
+        files = {'file': zpl.encode('utf-8')}
 
         response = requests.post(url, headers=headers, files=files, stream=True)
 
